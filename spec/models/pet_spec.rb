0ed4cb86da91c66f5ae707a5d6ea3a0774741819
= <=>
2024-07-23 19:14:32 -0300

    create specs for pets

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -9,7 +9,12 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-class Pet < ApplicationRecord
 
-  validates_presence_of :name, :kind
+require 'rails_helper'
+
+RSpec.describe Pet, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:kind) }
+  end
 end
